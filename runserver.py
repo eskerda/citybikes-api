8370bb78c65cc1ed3110296b755dcd2368805cff
eskerda <eskerda@gmail.com>
2013-08-19 18:35:34 +0200

    Define the port in which the dev server runs

--- runserver.py
+++ runserver.py
@@ -6,4 +6,4 @@ app.config['MONGO_HOST'] = config.MONGO_HOST
 app.config['MONGO_PORT'] = config.MONGO_PORT
 app.config['MONGO_DB'] = config.MONGO_DB
 
-app.run(debug=config.DEBUG)
+app.run(port = config.PORT, debug=config.DEBUG)
